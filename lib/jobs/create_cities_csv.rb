58188354e99d31dc0533ffea0ebc2122a2211ca0
Mauro Berlanda <m.berlanda@fit2you.it>
2016-02-25 16:53:17 +0100

    working but banned after 200 records

--- lib/jobs/create_cities_csv.rb
+++ lib/jobs/create_cities_csv.rb
@@ -1,6 +1,8 @@
 require_relative '../csv_parser'
 require_relative '../istat_header'
 require_relative '../city'
+require_relative '../scraper'
+require 'byebug'
 
 ROOT = File.expand_path("../..", File.join(File.dirname(__FILE__)))
 
@@ -12,17 +14,21 @@ v = istat.values
 n = v.size - 1
 
 
-headers = ["name", "region", "province", "plate", "cf"]
+headers = ["name", "region", "province", "plate", "cf", "cap"]
 values = []
 
 
 (0..n).each do |i|
   begin
     r = City.new(v[i], h)
-    values << [r.name, r.region, r.province, r.plate, r.cf]
+    s = Scraper.new(r)
+    cap = s.get_cap
+    values << [r.name, r.region, r.province, r.plate, r.cf, cap]
+    puts "#{r.name} #{r.plate} #{cap}"
   rescue Exception
-    print "row #{i} gave a problem"
+    puts "row #{i} gave a problem"
   end
+  sleep(Random.rand(50)/100.0)
 end
 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,18 +1,32 @@
 require 'uri'
+require 'open-uri'
+require 'nokogiri'
+require 'ostruct'
 
 class Scraper
 
   BASE_URL = 'http://www.paginebianche.it/cap/'
 
-  class << self
-
-    def create_url r
-      region = URI.escape(r.region.downcase)
-      plate = URI.escape(r.plate.downcase)
-      city = URI.escape(r.city.downcase)
-      BASE_URL + [region, plate, city].join('/') + '.html'
-    end
-
+  def initialize r
+    @r = r
   end
+
+  def get_cap
+    doc = Nokogiri::HTML(get_page_content)
+    doc.css("span[class='result-cont-li li-cap']").text
+  end
+
+  def get_page_content
+    url = create_url
+    source = open(url){|f|f.read}
+  end
+
+  def create_url
+    region = URI.escape(@r.region.downcase)
+    plate = URI.escape(@r.plate.downcase)
+    name = URI.escape(@r.name.downcase)
+    BASE_URL + [region, plate, name].join('/') + '.html'
+  end
+
 
 end
