c8b2bf83b7a790995d41baebf42272d591e08474
Mauro Berlanda <m.berlanda@fit2you.it>
2016-02-26 12:08:04 +0100

    implemented workaround to import from previous log

--- lib/parse_from_log.rb
+++ lib/parse_from_log.rb
@@ -1,10 +1,20 @@
 require_relative 'csv_parser'
 require_relative 'istat_header'
 require_relative 'city'
-require_relative 'scraper'
-require 'byebug'
 
-class ScrapeTask
+class CapLog
+  
+  attr_reader :list
+  ROOT = File.expand_path("..", File.join(File.dirname(__FILE__)))
+
+  def initialize file_name
+    file =  File.open(File.join(ROOT, 'data', file_name)).read
+    file.gsub!(/\r\n?/, "\n")
+    @list = file.split("\n")
+  end
+end
+
+class ParseFromLog
 
   ROOT = File.expand_path("..", File.join(File.dirname(__FILE__)))  
   HEADERS = ["name", "region", "province", "plate", "cf", "cap"]
@@ -13,40 +23,30 @@ class ScrapeTask
     istat = CsvParser.new('codici_istat_2016.csv')
     @h = IstatHeader.hash(istat.headers)
     @v = istat.values
+    @c = CapLog.new('log.txt').list
     n = @v.size - 1
     @range = (0..n)
     @values = []
     @errors = []
+    @log = File.open(File.join(ROOT, 'data', 'logger.txt'), "a")
   end
 
   def run
-
-    scrape_routine(@range)
-
-    while !@errors.empty? do
-      @range = @errors
-      @errors = []
-      scrape_routine(@range)
-    end
-
-    export
- 
-  end
-
-  def scrape_routine(index_range)
-    index_range.each do |i|
+    @range.each do |i|
       begin
         r = City.new(@v[i], @h)
-        s = Scraper.new(r)
-        cap = s.get_cap
+        cap = get_cap(r, i)
         @values << [r.name, r.region, r.province, r.plate, r.cf, cap]
         puts "#{r.name} #{r.plate} #{cap}"
+        log_to_file("#{r.name} #{r.plate} #{cap}\n")
       rescue Exception
         puts "#{r.name} #{r.plate} gave a problem"
+        log_to_file("#{r.name} #{r.plate} gave a problem\n")
         @errors << i
       end
-      sleep(4)
     end
+    @log.close
+    export
   end
 
   def compile_text
@@ -63,4 +63,18 @@ class ScrapeTask
     f.close
   end
 
+  def get_cap city, index
+    val = [city.name, city.plate].join(" ")
+    if @c[index].include?(val) 
+      @c[index].gsub(val, "").strip
+    else
+      byebug
+    end
+  end
+
+  def log_to_file text
+    @log.write(text)
+  end
+
 end
+
